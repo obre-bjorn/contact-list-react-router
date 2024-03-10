cdb762995e9add498c1fb1a1d02792c573e7ae20
obre-bjorn <obre.bjorn@gmail.com>
2024-03-10 13:31:43 +0300

    Nested routes

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,15 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root/>,
-    errorElement: <ErrorPage />
-  },{
-    path: 'contacts/:contactId',
-    element: <Contact/>,
-  }]);
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact/>,
+      }
+    ],
+  },
+]);
 
 
 
